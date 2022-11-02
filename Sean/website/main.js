984b43047e65362849af7182feeacd8dbc04c6b8
Sean-Shmulevich <zecoolioguy@gmail.com>
2022-11-01 20:54:29 -0400

    website menupage

---
+++ Sean/website/main.js
@@ -0,0 +1,22 @@
+
+
+window.addEventListener('load', (event) => {
+    window.onscroll = function() {myFunction()};
+
+
+    // Get the navbar
+    var navbar = document.querySelector(".menuBar");
+    console.log(navbar);
+    
+    // Get the offset position of the navbar
+    var sticky = navbar.clientTop;
+    
+    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    function myFunction() {
+      if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+      } else {
+        navbar.classList.remove("sticky");
+      }
+    }
+});
