97899c7346d4fe4f8c01c7c821a0deca3c883c44
Sean-Shmulevich <zecoolioguy@gmail.com>
2022-11-09 11:36:44 -0500

    website

--- html_css/main.js
+++ html_css/main.js
@@ -1,22 +1,21 @@
 
 
 window.addEventListener('load', (event) => {
-    window.onscroll = function() {myFunction()};
+    let a = document.querySelector(".menubutton");
+    a.addEventListener('click', (e) => {
+      console.log("here");
+      let menu = document.querySelectorAll(".expandingMenuItem");
 
+      if ((menu[0]).style.display === 'none')
+      {
 
-    // Get the navbar
-    var navbar = document.querySelector(".menuBar");
-    console.log(navbar);
-    
-    // Get the offset position of the navbar
-    var sticky = navbar.clientTop;
-    
-    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-    function myFunction() {
-      if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-      } else {
-        navbar.classList.remove("sticky");
+        menu.forEach(menuItem => menuItem.style.display = 'unset');
       }
-    }
+      else
+      {
+        menu.forEach(menuItem => menuItem.style.display = 'none');
+      }
+    });
+
+
 });
