1a03bc028d2c797fc6735cc9a70bfbd153dd237d
Sean-Shmulevich <zecoolioguy@gmail.com>
2022-11-10 22:39:59 -0500

    story viewer page looking very good not that buggy

--- html_css/js/bookshelf.js
+++ html_css/js/bookshelf.js
@@ -44,19 +44,21 @@ window.addEventListener('load', (event) => {
     tops[i].style.top = `${280 - randomHeight}px`;
     });
 
-    let shelfList = document.querySelector(".bookshelf");
+    // CODE I WROTE
+
+    let bookshelf = document.querySelector(".bookshelf");
 
 
-    shelfList.style.setProperty('--hoverRx', '0deg');
-    shelfList.style.setProperty('--hoverRy', '-60deg');
-    shelfList.style.setProperty('--hoverTy', '-10px');
-    shelfList.style.setProperty('--hoverTx', '-120px');
+    bookshelf.style.setProperty('--hoverRx', '0deg');
+    bookshelf.style.setProperty('--hoverRy', '-60deg');
+    bookshelf.style.setProperty('--hoverTy', '-10px');
+    bookshelf.style.setProperty('--hoverTx', '-120px');
 
-    shelfList.addEventListener('click', (event) => {
-        let clickedBook = event.target.parentElement;
+    bookshelf.addEventListener('click', (event) => {
+        var clickedBook = event.target.parentElement;
         // Catch the case where the user clicks on the bookshelf but between the books
         // in this case the parent of the bookshelf is the body
-        if(clickedBook === document.querySelector("body") || shelfList.classList.contains("minimizeBooks")){
+        if(clickedBook === document.querySelector("body") || bookshelf.classList.contains("minimizeBooks")){
             return;
         }
         let boundingclient = clickedBook.getBoundingClientRect();
@@ -64,20 +66,40 @@ window.addEventListener('load', (event) => {
         clickedBook.style.top = boundingclient.top+"px";
         clickedBook.style.left = boundingclient.left+"px";
         let temp = clickedBook.cloneNode(true);
-        shelfList.removeChild(clickedBook);
+        bookshelf.removeChild(clickedBook);
         document.querySelector("body").appendChild(temp);
         //We have clicked on a book. 
         //open it and move the books out of the way
-        shelfList.classList.add("minimizeBooks");
-        shelfList.style.justifyContent = "center";
-        shelfList.style.gridTemplateColumns = "10% 10% 10%";
-        shelfList.style.marginTop = "0";
+        bookshelf.classList.add("minimizeBooks");
+        bookshelf.style.justifyContent = "center";
+        bookshelf.style.gridTemplateColumns = "fr fr fr fr" ;
+        bookshelf.style.marginTop = "0";
+
+
+        //CSS set variable for where the animation should go.
+        bookshelf.style.setProperty('--xPos', `${-10}px`);
+        bookshelf.style.setProperty('--yPos', '-120px');
 
         //smaller bookshelf has different hover behaviors
-        shelfList.style.setProperty('--hoverRx', '-10deg');
-        shelfList.style.setProperty('--hoverRy', '-10deg');
-        shelfList.style.setProperty('--hoverTy', '-20px');
-        shelfList.style.setProperty('--hoverTx', '0px');
+        bookshelf.style.setProperty('--hoverRx', '-10deg');
+        bookshelf.style.setProperty('--hoverRy', '-10deg');
+        bookshelf.style.setProperty('--hoverTy', '-20px');
+        bookshelf.style.setProperty('--hoverTx', '0px');
+
+        bookshelf.style.marginBottom = "-120%";
+        document.querySelector("div.smallShelf").style.display = "unset";
+        document.querySelector("div.smallShelf").insertBefore(bookshelf, document.querySelector("div.smallShelf").firstChild);
+        bookshelf.addEventListener('animationstart', (e) => {
+          console.log('Animation ended');
+
+          console.log(window.innerWidth , clickedBook.style.left);
+          temp.style.transform = `translateY(-${boundingclient.top}px) translateX(${window.innerWidth*0.86 - parseInt(clickedBook.style.left)}px) rotateY(-60deg)`;
+          //put the books in the div in the bottom. 
+          //start the single book animation
+        });
+        bookshelf.addEventListener('animationend', (e) => {
+          document.querySelector("div.storyText").style.display = 'unset';
+        });
     }, false);
 });
 
