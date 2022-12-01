10a709037d1798b79b2266f21060c94a171c363b
Sean-Shmulevich <zecoolioguy@gmail.com>
2022-12-01 00:38:15 -0500

    woah that was a big tangle i got into

--- html_css/js/bookshelf.js
+++ html_css/js/bookshelf.js
@@ -54,33 +54,45 @@ window.addEventListener('load', (event) => {
     bookshelf.style.setProperty('--hoverTy', '-10px');
     bookshelf.style.setProperty('--hoverTx', '-120px');
 
+    let scale = .7;
+    if(window.innerWidth < 940){
+      scale = 0.1; 
+    }
+
     bookshelf.addEventListener('click', (event) => {
+      document.querySelector("#storyTitle").style.display = "none";
+      document.querySelector("#storyPage").style.transition = "500ms";
+      document.querySelector("#storyPage").style.backdropFilter = "blur(5px)";
         var clickedBook = event.target.parentElement;
+        // console.log(clickedBook);
+        // console.log(event.target);
+
         // Catch the case where the user clicks on the bookshelf but between the books
         // in this case the parent of the bookshelf is the body
-        if(clickedBook === document.querySelector("body") || bookshelf.classList.contains("minimizeBooks")){
+        if(clickedBook === document.querySelector("body") || bookshelf.classList.contains("minimizeBooks") || clickedBook === document.querySelector("main") || clickedBook === bookshelf){
             return;
         }
         let boundingclient = clickedBook.getBoundingClientRect();
-        clickedBook.style.position = "absolute";
+        // clickedBook.style.position = "absolute";
         clickedBook.style.top = boundingclient.top+"px";
         clickedBook.style.left = boundingclient.left+"px";
-        let temp = clickedBook.cloneNode(true);
         bookshelf.removeChild(clickedBook);
-        document.querySelector("body").appendChild(temp);
+        let temp = clickedBook.cloneNode(true);
+        document.querySelector("#readingViewContentBox").appendChild(temp);
         //We have clicked on a book. 
         //open it and move the books out of the way
         bookshelf.classList.add("minimizeBooks");
         bookshelf.style.justifyContent = "center";
         bookshelf.style.gridTemplateColumns = "fr fr fr fr" ;
         bookshelf.style.marginTop = "0";
+        bookshelf.style.position = "unset";
 
         document.querySelector("#menubutton").style.zIndex="20";
         document.querySelector("#homebutton").style.zIndex="20";
 
 
         //CSS set variable for where the animation should go.
-        bookshelf.style.setProperty('--xPos', `${-13}px`);
+        bookshelf.style.setProperty('--xPos', `${-35}px`);
         bookshelf.style.setProperty('--yPos', '-120px');
 
         //smaller bookshelf has different hover behaviors
@@ -92,17 +104,35 @@ window.addEventListener('load', (event) => {
         bookshelf.style.marginBottom = "-120%";
         document.querySelector("div.smallShelf").style.display = "unset";
         document.querySelector("div.smallShelf").insertBefore(bookshelf, document.querySelector("div.smallShelf").firstChild);
+
+        //unset the media query stuff for small screens
+        if(scale === .1){
+          temp.style.display = "unset";
+        }
         bookshelf.addEventListener('animationstart', (e) => {
           console.log('Animation ended');
-
-          console.log(window.innerWidth , clickedBook.style.left);
-          temp.style.transform = `translateY(-${boundingclient.top}px) translateX(${window.innerWidth*0.86 - parseInt(clickedBook.style.left)}px) rotateY(-60deg)`;
+          temp.style.left = "200px";
+          temp.style.bottom = "300px";
+          console.log(boundingclient);
+          temp.style.transform = `translateX(${window.innerWidth - 605}px)  translateY(-${boundingclient.top - boundingclient.height/10}px) scale(${scale})  rotateY(-60deg)`;
           //put the books in the div in the bottom. 
           //start the single book animation
         });
         bookshelf.addEventListener('animationend', (e) => {
           document.querySelector("div.storyText").style.display = 'unset';
+
+          temp.style.top = temp.getBoundingClientRect().top - (temp.getBoundingClientRect().height*.21) + 'px';
+          temp.style.left = "-80px";
+          temp.style.position = "relative";
+          temp.style.transition = "none";
+          temp.style.marginLeft = "auto";
+          if(scale === .1){
+            temp.style.display = "none";
+            return;
+          }
+          temp.style.transform = `translateY(0px) translateX(-20px) scale(${scale}) rotateY(-60deg)`;
         });
+
     }, false);
 });
 
--- html_css/js/main.js
+++ html_css/js/main.js
@@ -9,20 +9,42 @@ window.addEventListener('load', (event) => {
     let menu = document.querySelector(".mobileMenu>div");
     let menuItems = document.querySelector(".mobileMenu>div>table");
     let mainContent = document.querySelector("main");
+
+    let moveBooks = document.querySelector(".smallShelf");
     console.log(menuItems);
 
-    if ((menuItems).style.display === 'none')
+    let path = window.location.pathname;
+    let page = path.split("/").pop();
+    console.log(menu.style.width);
+    //if the menu is not showing or if i have not set the style property yet.
+    if (menu.style.width === '0px' || !menu.style.width)
     {
-      console.log("here");
+
+      if(page === "stories.xhtml"){
+        mainContent.style.transform = "translateX(200px)";
+        moveBooks.style.marginLeft = "10px";
+      }
+      else{
+        mainContent.style.marginLeft = "205px";
+      }
+
       menuItems.style.display = 'unset';
       menu.style.width = "215px";
-      mainContent.style.marginLeft = "205px";
     }
     else
     {
-      menuItems.style.display = 'none';
-      menu.style.width = "0";
+      // menuItems.style.display = 'none';
+      menu.style.width = "0px";
       mainContent.style.marginLeft = "0";
+
+      //on the stories page the xslt picker bar needs to be shifted and also the storied page has a different way of moving the main content
+      if(page === "stories.xhtml"){
+        mainContent.style.transform = "translateX(0px)";
+        moveBooks.style.marginLeft = "0px";
+      }
+      // setTimeout(() => {
+      //   menuItems.style.display = 'none';
+      // }, 1000)
     }
   });
 
